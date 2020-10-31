1524f1874e1f391103689e18ad04fb3dc5dc7132
Angelika Modrzejewska <ang.modrzejewska@gmail.com>
2020-10-31 21:23:09 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './js/app.js',
+
+    mode: 'development',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+        publicPath: '/'
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        compress: true,
+        port: 3000,
+        hot: true,
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$|\.scss$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                ]
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img/',
+                            publicPath: '../img/'
+                        }
+                    }
+                ]
+            },
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({ template: './index.html' }),
+        new webpack.HotModuleReplacementPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'styles/[name].css',
+            chunkFilename: 'styles/chunks/[id].css'
+        }),
+        new CopyPlugin({
+            patterns: [{
+                from: 'public/img',
+                to: 'img'
+            }]
+
+        })
+    ]
+};
